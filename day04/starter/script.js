ab1871adcbad4ec93f1926031af989a33e1c72c3
Ethan Kwon <ethan200304@gmail.com>
2021-07-15 13:35:06 -0400

    finish mild goals

--- day04/starter/script.js
+++ day04/starter/script.js
@@ -3,23 +3,23 @@ console.log("script running");
 // Fix these querySelectors so that they each select the correct element.
 const englishTile = document.querySelector("#English");
 const spanishTile = document.querySelector("#Spanish");
-const chineseTile = document.querySelector("#chinese");
-const tagalogTile = querySelector("#Tagalog");
-const vietnameseTile = document.queryselector("#Vietnamese");
-const arabicTile = document.querySelector("#");
+const chineseTile = document.querySelector("#Chinese");
+const tagalogTile = document.querySelector("#Tagalog");
+const vietnameseTile = document.querySelector("#Vietnamese");
+const arabicTile = document.querySelector("#Arabic");
 const frenchTile = document.querySelector("#French");
-const koreanTile = document.querySelector("Korean");
-const russianTile = document.querySelector("#Russiian");
-
-console.log(englishTile);
-console.log(spanishTile);
-console.log(chineseTile);
-console.log(tagalogTile);
-console.log(vietnameseTile);
-console.log(arabicTile);
-console.log(frenchTile);
-console.log(koreanTile);
-console.log(russianTile);
+const koreanTile = document.querySelector("#Korean");
+const russianTile = document.querySelector("#Russian");
 
 // Add the id of the input field so we can access it!
-const inputField = document.querySelector("#");
+const inputField = document.querySelector("#guess");
+
+inputField.addEventListener("change", () => {
+  console.log("Guess submitted");
+  let guess = inputField.value;
+  if (document.querySelector("#" + guess.charAt(0).toUpperCase() + guess.slice(1)) !== null) {
+    tile = document.querySelector("#" + guess.charAt(0).toUpperCase() + guess.slice(1))
+    tile.classList.remove("hidden");
+  }
+  inputField.value = "";
+});
